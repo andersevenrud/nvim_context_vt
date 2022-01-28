b3d7072ab3a6db02ad173204b4420c1bbc46c2df
Anders Evenrud <andersevenrud@gmail.com>
2022-01-28 23:58:53 +0100

    refactor!: split source files (#41)
    Splits up and decouples some of the codebase in order to improve
    maintainability, testing and extension support.
    
    If you're using a custom parser, validator or resolver with requires
    from this plugin then this is a BREAKING CHANGE. Update your require
    path to `nvim_context_vt.utils`.

---
+++ lua/nvim_context_vt/config.lua
@@ -0,0 +1,134 @@
+local M = {}
+
+M.default_opts = {
+    min_rows = 1,
+    custom_parser = nil,
+    custom_validator = nil,
+    custom_resolver = nil,
+    highlight = 'ContextVt',
+    disable_ft = {},
+    disable_virtual_lines = false,
+    disable_virtual_lines_ft = {},
+    prefix = '-->',
+}
+
+M.targets = {
+    'function',
+    'method_declaration',
+    'function_declaration',
+    'function_definition',
+    'lexical_declaration',
+    'local_function',
+    'arrow_function',
+    'method_definition',
+    'arguments',
+
+    'try_statement',
+    'catch_clause',
+    'finally_clause',
+
+    'if_statement',
+    'if_expression',
+
+    'switch_expression',
+
+    'class_definition',
+    'class_declaration',
+    'struct_expression',
+
+    'test_expression',
+
+    'while_expression',
+    'while_statement',
+
+    'for_expression',
+    'foreach_statement',
+    'for_statement',
+    'for_in_statement',
+
+    'with_statement',
+
+    -- rust
+    'match_expression',
+    'if_let_expression',
+    'tuple_struct_pattern',
+    'while_let_expression',
+    'loop_expression',
+    'function_item',
+    'struct_item',
+    'unsafe_block',
+
+    -- ruby target
+    'class',
+    'module',
+    'method',
+    'do_block',
+    'if',
+    'while',
+    'for',
+
+    -- typescript/javascript
+    'interface_declaration',
+    'enum_declaration',
+    'call_expression',
+    'export_statement',
+    'property_signature',
+
+    -- lua,
+    'local_variable_declaration',
+    'variable_declaration',
+
+    -- go
+    'type_declaration',
+    'type_spec',
+    'short_var_declaration',
+    'defer_statement',
+    'expression_switch_statement',
+    'composite_literal',
+    'element',
+    'func_literal',
+    'go_statement',
+    'select_statement',
+    'communication_case',
+    'default_case',
+
+    -- cpp
+    'case_statement',
+    'for_range_loop',
+
+    -- python
+    'list',
+    'pair',
+    'assignment',
+    'decorated_definition',
+
+    -- yaml
+    'block_mapping_pair',
+}
+
+M.ignore_root_targets = {
+    'program',
+    'document',
+}
+
+M.line_targets = {
+    'function_definition',
+    'class_definition',
+    'if_statement',
+    'try_statement',
+    'with_statement',
+    'for_statement',
+
+    -- python
+    'decorated_definition',
+
+    -- yaml
+    'block_mapping_pair',
+}
+
+M.line_ft = {
+    'python',
+    'yaml',
+}
+
+return M
---
+++ lua/nvim_context_vt/init.lua
@@ -0,0 +1,57 @@
+local parsers = require('nvim-treesitter.parsers')
+local config = require('nvim_context_vt.config')
+local utils = require('nvim_context_vt.utils')
+
+local M = {}
+local ns = vim.api.nvim_create_namespace('context_vt')
+local opts = config.default_opts
+
+function M.setup(user_opts)
+    opts = vim.tbl_extend('force', config.default_opts, user_opts or {})
+end
+
+function M.show_debug()
+    local ft = parsers.get_buf_lang()
+    local result = utils.find_virtual_text_nodes(function()
+        return true
+    end, ft, opts)
+
+    local values = vim.tbl_values(result)
+    local lines = vim.tbl_keys(result)
+
+    for index, nodes in ipairs(values) do
+        local prefix = string.rep('   ', #values - index)
+        local line = lines[index]
+
+        for _, node in ipairs(nodes) do
+            print(prefix .. line .. ':' .. node:type() .. ' ' .. utils.default_parser(node, ft, opts))
+        end
+    end
+end
+
+function M.show_context()
+    local ft = parsers.get_buf_lang()
+    if vim.tbl_contains(opts.disable_ft, ft) then
+        return
+    end
+
+    local validate = opts.custom_validator or utils.default_validator
+    local resolve = opts.custom_resolver or utils.default_resolver
+    local parse = opts.custom_parser or utils.default_parser
+
+    vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+
+    local result = utils.find_virtual_text_nodes(validate, ft, opts)
+    local create_vt = utils.create_virtual_text_factory(parse, ft, opts)
+
+    for line, nodes in pairs(result) do
+        local node = resolve(nodes, ft, opts)
+        local vt = create_vt(node, nodes)
+
+        if vt then
+            vim.api.nvim_buf_set_extmark(0, ns, line, 0, vt)
+        end
+    end
+end
+
+return M
