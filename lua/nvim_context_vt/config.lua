54a91761d340292b3436f22634ab3743156e24a9
Anders Evenrud <andersevenrud@gmail.com>
2022-02-04 02:34:29 +0100

    feat: improve html, add vue and jsx support (#50)

--- lua/nvim_context_vt/config.lua
+++ lua/nvim_context_vt/config.lua
@@ -87,7 +87,6 @@ M.targets = {
     'defer_statement',
     'expression_switch_statement',
     'composite_literal',
-    'element',
     'func_literal',
     'go_statement',
     'select_statement',
@@ -117,6 +116,15 @@ M.targets = {
     -- bash
     'case_item',
     'elif_clause',
+
+    -- html / vue / jsx
+    'element',
+    'template_element',
+    'style_element',
+    'script_element',
+    'jsx_element',
+    'jsx_expression',
+    'jsx_fragment',
 }
 
 M.ignore_root_targets = {
